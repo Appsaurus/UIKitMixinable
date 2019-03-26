b6258a396c40964dec4285d863c7fb75afe11a36
Brian Strobach <brian@appsaurus.io>
2019-03-26 15:42:06 -0500

    WIP Dependency updates

--- Sources/Shared/Mixinable/AppDelegate/UIApplicationDelegateMixinable.swift
+++ Sources/Shared/Mixinable/AppDelegate/UIApplicationDelegateMixinable.swift
@@ -19,6 +19,20 @@ extension UIApplicationDelegateLifeCycle {
 
 open class UIApplicationDelegateMixin<Mixable>: Mixin<Mixable> & UIApplicationDelegateLifeCycle{
     open func didInit() {}
+
+    open func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+        print("NOTIFICATION \(notification)")
+    }
+    open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print("NOTIFICATION \(userInfo)")
+    }
+    open func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("deviceToken \(deviceToken)")
+    }
+
+    open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("NOTIFICATION \(userInfo)")
+    }
 }
 
 public protocol UIApplicationDelegateMixinable: Mixinable {}
