58c2280fe7e3476ea6cc93e410ff439ba5dcf834
Brian Strobach <brian@appsaurus.io>
2019-03-26 19:03:16 -0500

    More cleanup

--- Sources/Shared/Mixinable/AppDelegate/UserNotificationCenterDelegateMixinable.swift
+++ Sources/Shared/Mixinable/AppDelegate/UserNotificationCenterDelegateMixinable.swift
@@ -11,36 +11,16 @@ import UserNotifications
 
 @available(iOS 10, *)
 open class UNUserNotificationCenterDelegateMixin<Mixable>: UIApplicationDelegateMixin<Mixable>, UNUserNotificationCenterDelegateLifeCycle{
-    open override func didInit() {
-        super.didInit()
-//        UNUserNotificationCenter.current().delegate = self
-    }
 
-    open func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-print("GOTEM")
-    }
+    open func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {}
 
-    open func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        print("GOTEM")
-    }
+    open func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {}
 
-    open func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
-print("GOTEM")
-    }
-//    open func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void){}
-//    open func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void){}
-//
-//    @available(iOS 12.0, *)
-//    open func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?){}
+    open func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {}
 }
 
 @available(iOS 10, *)
-public protocol UNUserNotificationCenterDelegateLifeCycle: UNUserNotificationCenterDelegate, UIApplicationDelegateLifeCycle{
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
-//    @available(iOS 12.0, *)
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?)
-}
+public protocol UNUserNotificationCenterDelegateLifeCycle: UNUserNotificationCenterDelegate, UIApplicationDelegateLifeCycle{}
 
 @available(iOS 10, *)
 public protocol UNUserNotificationCenterDelegateMixinable: UIApplicationDelegateMixinable & UNUserNotificationCenterDelegate {
