29d483b16bfa0b00de6e5d959d39ae3eba6cc689
Brian Strobach <brian@appsaurus.io>
2019-04-20 20:04:37 -0500

    bump 0.0.23

--- Sources/Shared/Mixinable/UIViewControllerMixinable.swift
+++ Sources/Shared/Mixinable/UIViewControllerMixinable.swift
@@ -127,3 +127,96 @@ extension Array where Element: UIViewControllerLifeCycle{
         forEach{$0.didReceiveMemoryWarning()}
     }
 }
+
+//public protocol UIViewControllerMixinable: SuperviewMixinable{
+//    func loadAsyncData()
+//}
+//
+//extension UIViewControllerMixinable {
+//    public func mix_viewDidLoadLifecycle(){
+//        mix_superviewLifecycle()
+//        mix_viewDidLoad()
+//        loadAsyncData()
+//    }
+//}
+//extension UIViewControllerMixinable {
+//
+//    public var viewControllerMixins: [UIViewControllerLifeCycle]{
+//        return mixins.map{$0 as? UIViewControllerLifeCycle}.compactMap{$0}
+//    }
+//    public func mix_loadView(){
+//        viewControllerMixins.forEach{$0.loadView()}
+//    }
+//    public func mix_loadViewIfNeeded(){
+//        viewControllerMixins.forEach{$0.loadViewIfNeeded()}
+//    }
+//    public func mix_viewDidLoad(){
+//        viewControllerMixins.forEach{$0.viewDidLoad()}
+//    }
+//    public func mix_loadAsyncData(){
+//        viewControllerMixins.forEach{$0.loadAsyncData()}
+//    }
+//    public func mix_prepare(for segue: UIStoryboardSegue, sender: Any?){
+//        viewControllerMixins.forEach{$0.prepare(for: segue, sender: sender)}
+//    }
+//    public func mix_viewWillAppear(_ animated: Bool){
+//        viewControllerMixins.forEach{$0.viewWillAppear(animated)}
+//    }
+//    public func mix_viewDidAppear(_ animated: Bool){
+//        viewControllerMixins.forEach{$0.viewDidAppear(animated)}
+//    }
+//    public func mix_viewWillDisappear(_ animated: Bool){
+//        viewControllerMixins.forEach{$0.viewWillDisappear(animated)}
+//    }
+//    public func mix_viewDidDisappear(_ animated: Bool){
+//        viewControllerMixins.forEach{$0.viewDidDisappear(animated)}
+//    }
+//    public func mix_viewWillLayoutSubviews(){
+//        viewControllerMixins.forEach{$0.viewWillLayoutSubviews()}
+//    }
+//    public func mix_viewDidLayoutSubviews(){
+//        viewControllerMixins.forEach{$0.viewDidLayoutSubviews()}
+//    }
+//    public func mix_didReceiveMemoryWarning(){
+//        viewControllerMixins.forEach{$0.didReceiveMemoryWarning()}
+//    }
+//
+//}
+//
+//extension Array where Element: UIViewControllerLifeCycle{
+//    public func loadView(){
+//        forEach{$0.loadView()}
+//    }
+//    public func loadViewIfNeeded(){
+//        forEach{$0.loadViewIfNeeded()}
+//    }
+//    public func viewDidLoad(){
+//        forEach{$0.viewDidLoad()}
+//    }
+//    public func prepare(for segue: UIStoryboardSegue, sender: Any?){
+//        forEach{$0.prepare(for: segue, sender: sender)}
+//    }
+//    public func viewWillAppear(_ animated: Bool){
+//        forEach{$0.viewWillAppear(animated)}
+//    }
+//    public func viewDidAppear(_ animated: Bool){
+//        forEach{$0.viewDidAppear(animated)}
+//    }
+//    public func viewWillDisappear(_ animated: Bool){
+//        forEach{$0.viewWillDisappear(animated)}
+//    }
+//    public func viewDidDisappear(_ animated: Bool){
+//        forEach{$0.viewDidDisappear(animated)}
+//    }
+//    public func viewWillLayoutSubviews(){
+//        forEach{$0.viewWillLayoutSubviews()}
+//    }
+//    public func viewDidLayoutSubviews(){
+//        forEach{$0.viewDidLayoutSubviews()}
+//    }
+//    public func didReceiveMemoryWarning(){
+//        forEach{$0.didReceiveMemoryWarning()}
+//    }
+//}
+
+
