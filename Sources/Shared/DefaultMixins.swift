ee81c59b7986a9482e778cc28c966820ffee71b6
Brian Strobach <brian@appsaurus.io>
2019-12-03 13:59:37 -0600

    Stripping default mixing

--- Sources/Shared/DefaultMixins.swift
+++ Sources/Shared/DefaultMixins.swift
@@ -6,110 +6,85 @@
 //
 
 import UIKit
-open class DefaultViewMixin: UIViewMixin<UIView>{
-
-    open override func initProperties() {
-        super.initProperties()
-//        mixable.layoutMargins = .zero
-//        if #available(iOS 11.0, *) {
-//            mixable.directionalLayoutMargins = .zero
-//        }
-    }
-}
-
-open class DefaultViewControllerMixin: UIViewControllerMixin<UIViewController>{
-    open override func initProperties() {
-        super.initProperties()
-        mixable.automaticallyAdjustsScrollViewInsets = false
-        mixable.edgesForExtendedLayout = .all
-        if #available(iOS 11.0, *) {
-            mixable.additionalSafeAreaInsets = .zero
-        }
-    }
-}
 
 public typealias DefaultMixinsClosure<O: NSObjectProtocol> = (_ object: O) -> [LifeCycle]
 open class DefaultMixins{
 
     //MARK: ViewControllers
     static public var viewControllers: DefaultMixinsClosure<UIViewController> = { vc in
-        return [
-            DefaultViewControllerMixin(vc)
-        ]
+        return []
     }
-    
+
     static public var viewController: DefaultMixinsClosure<UIViewController> = { vc in
         return viewControllers(vc)
     }
-    
+
     static public var tableViewController: DefaultMixinsClosure<UITableViewController> = { vc in
         return viewControllers(vc)
     }
-    
+
     static public var collectionViewController: DefaultMixinsClosure<UICollectionViewController> = { vc in
         return viewControllers(vc)
     }
-    
+
     static public var tabBarController: DefaultMixinsClosure<UITabBarController> = { vc in
         return viewControllers(vc)
     }
-    
+
     static public var navigationController: DefaultMixinsClosure<UINavigationController> = { vc in
         return viewControllers(vc)
     }
-    
+
     static public var pageViewController: DefaultMixinsClosure<UIPageViewController> = { vc in
         return viewControllers(vc)
     }
-    
+
     //MARK: Views
-    
+
     static public var views: DefaultMixinsClosure<UIView> = { view in
-        return [
-            DefaultViewMixin(view)
-        ]
+        return []
     }
-    
+
     static public var view: DefaultMixinsClosure<UIView> = { view in
         return views(view)
     }
-    
+
     static public var button: DefaultMixinsClosure<UIButton> = { view in
         return views(view)
     }
-    
+
     static public var collectionViewCell: DefaultMixinsClosure<UICollectionViewCell> = { view in
         return views(view)
     }
-    
+
     static public var imageView: DefaultMixinsClosure<UIImageView> = { view in
         return views(view)
     }
-    
+
     static public var scrollView: DefaultMixinsClosure<UIScrollView> = { view in
         return views(view)
     }
-    
+
     static public var searchBar: DefaultMixinsClosure<UISearchBar> = { view in
         return views(view)
     }
-    
+
     static public var tableViewCell: DefaultMixinsClosure<UITableViewCell> = { view in
         return views(view)
     }
-    
+
     static public var tableViewHeaderFooterView: DefaultMixinsClosure<UITableViewHeaderFooterView> = { view in
         return views(view)
     }
-    
+
     static public var textField: DefaultMixinsClosure<UITextField> = { view in
         return views(view)
     }
-    
+
     static public var toolbar: DefaultMixinsClosure<UIToolbar> = { view in
         return views(view)
     }
-    
+
     //MARK: Other
     static public var barButtonItem: DefaultMixinsClosure<UIBarButtonItem> = { view in
         return []
